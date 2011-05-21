1659150398e6033c729cf872243b77a31446a699
Nitish Dodagatta <nitish@nishmu.com>
2011-05-21 05:56:36 +0530

    Name UI elements appropriately.

--- src/gnc/dashboard.cpp
+++ src/gnc/dashboard.cpp
@@ -1,5 +1,29 @@
+/*
+ * Copyright (C) 2011 Free Software Foundation, Inc
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * @file
+ * Dashboard for embedding various dock widgets.
+ */
+
 #include "dashboard.hpp"
 #include "ui_dashboard.h"
+
+namespace gnc
+{
 
 Dashboard::Dashboard(QWidget *parent) :
     QMainWindow(parent),
@@ -17,3 +41,5 @@ Dashboard::~Dashboard()
 {
     delete ui;
 }
+
+} // END namespace gnc
--- src/gnc/dashboard.hpp
+++ src/gnc/dashboard.hpp
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2011 Free Software Foundation, Inc
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * @file
+ * Dashboard for embedding various dock widgets.
+ */
+
 #ifndef DASHBOARD_HPP
 #define DASHBOARD_HPP
 
@@ -6,6 +27,9 @@
 namespace Ui {
     class Dashboard;
 }
+
+namespace gnc
+{
 
 class Dashboard : public QMainWindow
 {
@@ -18,5 +42,7 @@ public:
 private:
     Ui::Dashboard *ui;
 };
+
+} // END namespace gnc
 
 #endif // DASHBOARD_HPP
